dd355b5a678064e85b90f85f576ed0e122bc8cf7
ZyronHub0 <zyronproject@gmail.com>
2025-10-08 18:54:09 -0300

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/f3edc991504d46d7be0e20823ac2f674"))()
+loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/1af3283dc0cc63e1"))()
